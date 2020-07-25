4d38f6d21a0da24192b8cc3e15aa2c610f5160b3
Andrew Collins <andrewjcollins@live.com>
2020-07-25 16:30:46 -0500

    update contents to content

--- ssg.py
+++ ssg.py
@@ -1,7 +1,7 @@
 import typer 
 from ssg.site import Site
 
-def main(source="contents", dest="dist"):
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
     Site(**config).build()
 
